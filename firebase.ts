193c1df2e2f9cf23f2de2110a89eb9bc1bacc9b5
Haiderjwad <65730159+Haiderjwad@users.noreply.github.com>
2026-04-22 02:46:12 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID
+    apiKey: AIzaSyAbVcpuHlF-0znuWXU3IzGQzccpwtYthBs,
+    authDomain: so-system-dedd6.firebaseapp.com,
+    projectId: so-system-dedd6,
+    storageBucket: so-system-dedd6.firebasestorage.app,
+    messagingSenderId: 132679072651,
+    appId: 1:132679072651:web:2bce20903cc8d4c57c8d8e
 };
 
 const app = initializeApp(firebaseConfig);
