5787985714c20e48c2f5891a33b782debce04f89
Haiderjwad <65730159+Haiderjwad@users.noreply.github.com>
2026-04-22 02:49:39 +0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: AIzaSyAbVcpuHlF-0znuWXU3IzGQzccpwtYthBs,
-    authDomain: so-system-dedd6.firebaseapp.com,
-    projectId: so-system-dedd6,
-    storageBucket: so-system-dedd6.firebasestorage.app,
-    messagingSenderId: 132679072651,
-    appId: 1:132679072651:web:2bce20903cc8d4c57c8d8e
+  apiKey: "AIzaSyAbVcpuHlF-0znuWXU3IzGQzccpwtYthBs",
+  authDomain: "so-system-dedd6.firebaseapp.com",
+  projectId: "so-system-dedd6",
+  storageBucket: "so-system-dedd6.firebasestorage.app",
+  messagingSenderId: "132679072651",
+  appId: "1:132679072651:web:2bce20903cc8d4c57c8d8e"
 };
 
 const app = initializeApp(firebaseConfig);
